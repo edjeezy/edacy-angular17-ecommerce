dfec1014c0b1f200e657bd7d0bc2a6630ece2dfd
Mouhamed Mar <mouhamededji@gmail.com>
2025-08-08 17:28:51 +0000

    feat: Set up initial app layout with PrimeNG toolbar
    This commit establishes the basic structure and layout for the application.
    
    - Adds a main header using the PrimeNG Toolbar component, which includes the Edacy logo and navigation icons.
    - Creates initial placeholder components for the Home and Details pages and organizes them in a new `src/app/pages` directory.
    - Imports the necessary PrimeNG modules (`ToolbarModule`, `InputTextModule`) to support the new UI.
    - Updates the PrimeNG theme to `aura-light-amber` for a new visual style.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    ToolbarModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
