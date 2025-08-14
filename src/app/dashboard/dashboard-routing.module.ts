d2da916a0dbdb333b38c28388724b510b8e2d81a
Mouhamed Mar <mouhamededji@gmail.com>
2025-08-14 18:41:13 +0000

    ajout de pages dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AdminNotFoundComponent } from './pages/admin-not-found/admin-not-found.component';
+import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
+import { AdminSignupComponent } from './pages/admin-signup/admin-signup.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'profile' },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'login', component: AdminLoginComponent },
+      { path: 'signup', component: AdminSignupComponent },
+      { path: '**', component: AdminNotFoundComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
